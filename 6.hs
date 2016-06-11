89c334c701f28a6b5341d47dd062d7a031764428
Simon Lydell <simon.lydell@gmail.com>
2016-06-12 00:04:55 +0200

    Add solution for problem 6

---
+++ 6.hs
@@ -0,0 +1,18 @@
+main :: IO ()
+main =
+  print answer
+
+
+answer :: Int
+answer =
+  let
+    numbers =
+      [1..100]
+
+    sumOfSquares =
+      sum $ map (^ 2) numbers
+
+    squareOfSum =
+      (sum numbers) ^ 2
+  in
+    squareOfSum - sumOfSquares
