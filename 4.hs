8a64be6e8a44ba0b58b7f118d134a4f4da62f2af
Simon Lydell <simon.lydell@gmail.com>
2016-06-11 23:05:53 +0200

    Add solution for problem 4

---
+++ 4.hs
@@ -0,0 +1,43 @@
+main :: IO ()
+main =
+  print answer
+
+
+answer :: Int
+answer =
+  let
+    threeDigitNumbers =
+      [100..999]
+
+    threeDigitNumbersCombinations =
+      combinations threeDigitNumbers threeDigitNumbers
+
+    products =
+      map (\(a, b) -> a * b) threeDigitNumbersCombinations
+  in
+    maximum (filter isPalindrome products)
+
+
+combinations :: Eq b => [a] -> [b] -> [(a, b)]
+combinations list1 list2 =
+  if list2 == [] then
+    []
+  else
+    case list1 of
+      head : tail ->
+        (map ((,) head) list2) ++ (combinations tail (drop 1 list2))
+
+      _ ->
+        []
+
+
+isPalindrome :: Int -> Bool
+isPalindrome n =
+  let
+    str =
+      show n
+
+    half =
+      (length str) `div` 2
+  in
+    (take half str) == (take half (reverse str))
